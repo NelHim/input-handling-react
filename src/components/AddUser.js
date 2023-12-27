274ec984f8394d34a1f7ab54362457d3b4c77a0e
NelHim <n.himbaza@alustudent.com>
2023-12-27 17:46:14 +0200

    feat: Implement a reusable button component

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,48 +1,48 @@
 import React, { useState } from "react";
+import Card from "./UI/Card";
+import classes from './AddUser.module.css'
+import Button from "./UI/Button";
 
 function AddUser(props) {
   const [userInput, SetUserInput] = useState({ username: "", age: 0 });
 
-    const inputChangeHandler = (input, value) =>{
-        SetUserInput((prev) => {
-            return {
-                ...prev, 
-                [input]: value
-            }
-        })
-    }
+  const inputChangeHandler = (input, value) => {
+    SetUserInput((prev) => {
+      return {
+        ...prev,
+        [input]: value,
+      };
+    });
+  };
 
   const FormSubmithandler = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     props.onFormSubmission(userInput);
   };
-    
-    return (
+
+  return (
+    <Card className={classes.input} >
       <form onSubmit={FormSubmithandler}>
-        <div>
-          <label htmlFor="UserName">User Name</label>
-          <input
-            type="text"
-            id="username"
-            onChange={(event) => {
-              event.preventDefault();
-              inputChangeHandler("username", event.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <label htmlFor="Years">Age(Years)</label>
-          <input
-            type="number"
-            id="years"
-            onChange={(event) => {
-              inputChangeHandler("years", event.target.value);
-            }}
-          />
-        </div>
-        <button type="submit">Add User</button>
+        <label htmlFor="username">User Name</label>
+        <input
+          type="text"
+          id="username"
+          onChange={(event) => {
+            inputChangeHandler("username", event.target.value);
+          }}
+        />
+        <label htmlFor="Years">Age(Years)</label>
+        <input
+          type="number"
+          id="years"
+          onChange={(event) => {
+            inputChangeHandler("years", event.target.value);
+          }}
+        />
+        <Button type="submit">Add User</Button>
       </form>
-    );
+    </Card>
+  );
 }
 
 export default AddUser;
